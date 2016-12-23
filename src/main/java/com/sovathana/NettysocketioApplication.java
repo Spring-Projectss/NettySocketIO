d81c83f971ab93228312e0c5b9dfd0e6858122d6
Lun Sovathana <lunsovathana@Luns-Mac-mini.kshrd.com.kh>
2016-12-23 17:16:50 +0700

    Initialize

---
+++ src/main/java/com/sovathana/NettysocketioApplication.java
@@ -0,0 +1,12 @@
+package com.sovathana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettysocketioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettysocketioApplication.class, args);
+	}
+}
